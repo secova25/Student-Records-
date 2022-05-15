c70f7de86604036dc31831532300174ad70e5d66
Sergio Covarrubias <75351438+secova25@users.noreply.github.com>
2022-05-14 19:51:22 -0500

    Update INSERTSTOTABLES.sql

--- Code/Student Records/Documents/SQL/INSERTSTOTABLES.sql
+++ Code/Student Records/Documents/SQL/INSERTSTOTABLES.sql
@@ -1,22 +1,22 @@
-INSERT INTO userinfo(FNAME,MNAME,LNAME,USERNAME,PASSWORD1)
+INSERT INTO userinfo(fname,mname,lname,username,password)
 	VALUES('Samantha', 'T', 'Lurance', 'stlur23', 'student1@gmail.com');
 
-INSERT INTO userinfo(FNAME,MNAME,LNAME,USERNAME,PASSWORD1)
+INSERT INTO userinfo(fname,mname,lname,username,password)
 	VALUES('Tim', 'P', 'Queens', 'Tpque65', 'student2@gmail.com');
     
-INSERT INTO userinfo(FNAME,MNAME,LNAME,USERNAME,PASSWORD1)
+INSERT INTO userinfo(fname,mname,lname,username,password)
 	VALUES('Admin', 'profile', 'control', 'admin1', 'password');
     
-INSERT INTO records(FNAME,MNAME,LNAME,USERNAME,CLASSNAME,CREDITTYPE,CREDITVALUE)
+INSERT INTO records(fname,mname,lname,username,classname,credittype,creditamount)
 	VALUES('Samantha', 'T', 'Lurance', 'stlur23', 'HISTORY of Eastern Religion','COLLEGE',3);
  
-INSERT INTO records(FNAME,MNAME,LNAME,USERNAME,CLASSNAME,CREDITTYPE,CREDITVALUE)
+INSERT INTO records(fname,mname,lname,username,classname,credittype,creditamount)
 	VALUES('Samantha', 'T', 'Lurance', 'stlur23', 'Chemistry','HIGHSCHOOL',1);
-    
-INSERT INTO records(FNAME,MNAME,LNAME,USERNAME,CLASSNAME,CREDITTYPE,CREDITVALUE)
+   
+INSERT INTO records(fname,mname,lname,username,classname,credittype,creditamount)
 	VALUES('Tim', 'P', 'Queens', 'Tpque65', 'Introduction To Computer Science','COLLEGE',3);
  
-INSERT INTO records(FNAME,MNAME,LNAME,USERNAME,CLASSNAME,CREDITTYPE,CREDITVALUE)
+INSERT INTO records(fname,mname,lname,username,classname,credittype,creditamount)
 	VALUES('Tim', 'P', 'Queens', 'Tpque65', 'Algebra 1','HIGHSCHOOL',1);
     
 
